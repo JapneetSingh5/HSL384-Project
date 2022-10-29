96a80448fd44c5563b40fa8c541ede20afd9c584
Japneet Singh <singhjapneetssa@gmail.com>
2022-10-30 02:40:42 +0530

    added final script [getLyrics.py]

--- getLyrics.py
+++ getLyrics.py
@@ -7,17 +7,17 @@ token = 'BVUCPvV3k4iToDaobKg3khKmDJgFF1-92I5XSTJfvKo9tSCslVtXbV9dddCHyWRe';
 import lyricsgenius
 genius = lyricsgenius.Genius(token)
 
-df = pd.read_csv('hot100_cleaned.csv')
+df = pd.read_csv('hot100_cleaned_1989.csv')
 df_dict = df.to_dict('records')
-f=open('lyrics.csv', 'a')
-f.write('title,artist,lyrics\n')
+f=open('lyrics_1989_2022.csv', 'a')
 iter = 0
 success = 0
 failed = 0
 csvw = csv.writer(f, delimiter=',')
+csvw.writerow(['title', 'artist','lyrics'])
 for row in tqdm(df_dict):
     iter+=1
-    if(iter==100):
+    if(iter==10):
         break
     # print(row['artist'])
     artist = genius.search_artist(row['artist'], allow_name_change=True, max_songs=0)
